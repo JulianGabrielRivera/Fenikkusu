8e7bf29eda76b740018514a69af3bf03191b9a2d
Julian Rivera <julianarevircoins@gmail.com>
2025-11-30 17:28:45 -0500

    new images

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,13 +9,13 @@ const Header = async ({ user }: { user: User }) => {
     <header className="sticky top-0 header">
       <div className="container header-wrapper">
         <Link href="/">
-          <Image
+          {/* <Image
             src="/assets/icons/logo.svg"
             alt="stocks logo"
             width={140}
             height={32}
             className="h-8 w-auto cursor-pointer"
-          />
+          /> */}
         </Link>
         <nav className="hidden sm:block">
           <NavItems initialStocks={initialStocks} />
